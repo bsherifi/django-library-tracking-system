7ad058e936473413973b0ab99788db49db63f8fc
Betim Sherifi <betimsh99@gmail.com>
2025-04-01 21:25:24 +0200

    finished core skills & first celery periodic task

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,14 @@
 import random
-# rand_list =
 
-# list_comprehension_below_10 =
+rand_list = [random.randint(1, 100) for _ in range(10)]
 
-# list_comprehension_below_10 =
+
+# Using list comprehension
+list_comprehension_below_10 = [num for num in rand_list if num < 10]
+
+
+# Using filters
+def below_ten(num):
+    return num < 10
+
+list_comprehension_below_10 = list(filter(below_ten, rand_list))
